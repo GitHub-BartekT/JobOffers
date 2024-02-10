2601b07435666a8cc3fbd9e88f83e994a8fdefbb
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2024-02-10 22:01:54 +0100

    Add business requirements

--- src/main/java/pl/iseebugs/JobOffers/domain/offers/OffersFacade.java
+++
@@ -1,4 +0,0 @@
-package pl.iseebugs.JobOffers.domain.offers;
-
-public interface OffersFacade {
-}
--- src/test/java/pl/iseebugs/JobOffers/domain/offers/OffersFacadeTest.java
+++
@@ -1,7 +0,0 @@
-package pl.iseebugs.JobOffers.domain.offers;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class OffersFacadeTest {
-
-}
