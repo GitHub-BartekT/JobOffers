fb19ca0f0493c166a445d8ed570bc46fb1a7729a
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2024-02-19 11:02:47 +0100

    Add clietn properties

--- src/main/java/pl/iseebugs/JobOffers/JobOffersApplication.java
+++ src/main/java/pl/iseebugs/JobOffers/JobOffersApplication.java
@@ -2,8 +2,11 @@ package pl.iseebugs.JobOffers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import pl.iseebugs.JobOffers.infrastructure.security.http.offersAWS.OfferAWSFetcherClientProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({OfferAWSFetcherClientProperties.class})
 public class JobOffersApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/pl/iseebugs/JobOffers/infrastructure/security/http/offersAWS/OfferAWSFetcherClientProperties.java
@@ -0,0 +1,9 @@
+package pl.iseebugs.JobOffers.infrastructure.security.http.offersAWS;
+
+import lombok.Builder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "job-offers.offers-fetcher.http.client.config")
+@Builder
+public record OfferAWSFetcherClientProperties(String uri, int port, int readTimeout, int connectionTimeout) {
+}
