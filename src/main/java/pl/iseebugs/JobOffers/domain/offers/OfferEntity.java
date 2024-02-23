15a3a394f0e1d45739d433fc71dafb346aff070e
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2024-02-24 00:17:56 +0100

    Add Indexed in OffersRepository

--- src/main/java/pl/iseebugs/JobOffers/domain/offers/OfferEntity.java
+++ src/main/java/pl/iseebugs/JobOffers/domain/offers/OfferEntity.java
@@ -3,11 +3,12 @@ package pl.iseebugs.JobOffers.domain.offers;
 import lombok.Builder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.index.Indexed;
 
 @Builder
 @Document(collection = "savedOffers")
 record OfferEntity(
         @Id
         String id,
-        String url, String jobPosition, String companyName, double salaryLowerBound, double salaryUpperBound) {
+        @Indexed(unique = true) String url, String jobPosition, String companyName, double salaryLowerBound, double salaryUpperBound) {
 }
